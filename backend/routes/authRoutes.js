355c272c16adb00858a1e7e2a49b03510d3224fb
Agustin Vargas <agustinvargas11@gmail.com>
2016-10-27 19:18:15 -0400

    squash follow bug

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -35,10 +35,10 @@ authRoute.route('/signup')
                     } else if (!existingUser) {
                         var newUser = new User(req.body);
                         newUser.following.push('5811ff90a7eea95261661f96');
-                        User.findById('5811ff90a7eea95261661f96', function (err, admin) {
+                        User.findOne({_id: '5811ff90a7eea95261661f96'}, function (err, admin) {
                             if (err) res.status(500).send(err);
 
-                            admin.followers.push(newUser);
+                            admin.followers.push(newUser._id);
                             admin.save();
                         });
                         newUser.save(function (err, savedUser) {
